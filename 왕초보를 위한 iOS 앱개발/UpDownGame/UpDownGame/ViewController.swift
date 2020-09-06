ad6dbb3a982cc060cbbfb726bfea9a28aad30209
zziro12 <opers7@naver.com>
2020-09-06 15:13:22 +0900

    44/54 steps
    implement alert

--- 왕초보를 위한 iOS 앱개발/UpDownGame/UpDownGame/ViewController.swift
+++ 왕초보를 위한 iOS 앱개발/UpDownGame/UpDownGame/ViewController.swift
@@ -41,10 +41,42 @@ class ViewController: UIViewController {
     
     @IBAction func silderValueChanged(_ sender: UISlider) {
         print(sender.value)
+        let sliderValueInt: Int = Int(sender.value)
+        sliderValueLabel.text = String(sliderValueInt)
+    }
+    
+    func showAlert(message: String) {
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.reset()
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount += 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            showAlert(message: "YOU HIT!")
+            reset()
+        } else if tryCount >= 5 {
+            showAlert(message: "YOU Lose...")
+            reset()
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
