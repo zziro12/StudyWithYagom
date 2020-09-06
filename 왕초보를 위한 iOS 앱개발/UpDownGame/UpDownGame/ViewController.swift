42b1a934cbd81e5577782d82d6463dd825f0e2bd
zziro12 <opers7@naver.com>
2020-09-06 13:16:10 +0900

    35/54 steps

--- 왕초보를 위한 iOS 앱개발/UpDownGame/UpDownGame/ViewController.swift
+++ 왕초보를 위한 iOS 앱개발/UpDownGame/UpDownGame/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
         //image literal
+        reset()
     }
-
+    
+    func reset() {
+        print("reset")
+        randomValue = Int.random(in: 0...30)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+    }
+    
     @IBAction func silderValueChanged(_ sender: UISlider) {
         print(sender.value)
     }
@@ -29,6 +49,7 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        reset()
     }
 }
 
