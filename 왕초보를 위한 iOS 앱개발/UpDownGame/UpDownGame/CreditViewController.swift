82fc01fbf029b2c69ba2fc6ec225280f638e58a2
zziro12 <opers7@naver.com>
2020-09-06 17:08:46 +0900

    54/54steps finish:)

---
+++ 왕초보를 위한 iOS 앱개발/UpDownGame/UpDownGame/CreditViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by Jinho Choi on 2020/09/06.
+//  Copyright © 2020 Jinho Choi. All rights reserved.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
